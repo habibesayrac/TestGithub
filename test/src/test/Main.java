b77f76a020d8c24a046091b6964e1500742c54d5
Habibe Sayrac <hsayracode@gmail.com>
2022-10-30 13:45:08 +0300

    Commit 2 product added

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -2,7 +2,7 @@ package test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello Github");
+		System.out.println("Hello Github 11111 commit");
 	}
 
 }
---
+++ test/src/test/Product.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Product {
+
+	int id;
+	double price;
+}
